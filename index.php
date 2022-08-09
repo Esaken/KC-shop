4f8f20eb6c5a40a929f220cb64c64a3b1ccfabdd
code-monarch <kenesau4763@gmail.com>
2022-08-09 18:04:36 +0300

    faced a bootstrap css error, downloaded a new bootstrap file and done with top nav

--- index.php
+++ index.php
@@ -1,4 +1,65 @@
-<?php
-$txt = "PHP";
-echo "I love $txt!";
-?>
+<!DOCTYPE html> <html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>KC-SHOP</title>
+    <!--CSS style links start-->
+    <link rel="stylesheet" href="styles/bootstrap-337.min.css">
+    <!--<link rel="stylesheet" href="styles/bootstrap.min.css">-->
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles/styles.css">
+    <!--CSS style links END-->
+</head>
+<body>
+    
+<!--creating a top menu-->
+<div id="top"><!--top begin-->
+
+<div class="container"><!--container start-->
+
+<div class="col-md-6 offer"><!--offer start-->
+    <a href="#" class="btn btn-success btn-sm">Welcome</a>
+    <a href="checkout.php">4 items in your cart | Total Price: 300Ksh. </a>
+</div><!--offer start-->
+
+<div class="col-md-6"><!--   begin -->
+<ul class="menu"> <!-- start top menu-->
+<li>
+    <a href="customer_register.php">Register</a>
+</li>
+<li>
+    <a href="checkout.php">My Account</a>
+</li>
+<li>
+    <a href="cart.php">To Cart</a>
+</li>
+<li>
+    <a href="checkout.php">Login</a>
+</li>
+
+
+</ul><!--close top menu-->
+</div><!--   end-->
+
+
+
+</div><!--container end-->
+
+
+
+</div><!--top finish-->
+
+
+
+
+
+
+
+
+<!--SCRIPT start-->
+        <script src="js/Jquery-3.6.0.min.js"></script>
+        <script src="js/bootstrap.bundle.min.js"></script>
+<!--SCRIPT end-->
+</body>
+</html>
