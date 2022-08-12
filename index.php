87c5e6769e08aaa7789174a131d85402c32cba7b
code-monarch <kenesau4763@gmail.com>
2022-08-12 22:51:30 +0300

    started a fresh, put up a nav bar and slider

--- index.php
+++ index.php
@@ -5,97 +5,101 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>KC-SHOP</title>
     <!--CSS style links start-->
+    
+    <link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet">
 
     <link rel="stylesheet" href="styles/bootstrap.min.css">
     <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="styles/styles.css">
     <!--CSS style links END-->
 </head>
-<body>
+ <body class="p-3 m-0 border-0 bd-example">
     
-<!--creating a top menu-->
-<div id="top"><!--top begin-->
-
-<div class="container"><!--container start-->
-
-<div class="col-md-6 offer"><!--offer start-->
-    <a href="#" class="btn btn-success btn-sm">Welcome</a>
-    <a href="checkout.php">4 items in your cart | Total Price: 3000Ksh. </a>
-</div><!--offer start-->
-
-<div class="col-md-6"><!--   begin -->
-<ul class="menu"> <!-- start top menu-->
-<li>
-    <a href="customer_register.php">Register</a>
-</li>
-<li>
-    <a href="checkout.php">My Account</a>
-</li>
-<li>
-    <a href="cart.php">To Cart</a>
-</li>
-<li>
-    <a href="checkout.php">Login</a>
-</li>
 
 
-</ul><!--close top menu-->
-</div><!--   end-->
+<!--navbar start-->
+<nav class="navbar bg-light fixed-top">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">KC-shop navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+          <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">KC-shop</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+          <div class="offcanvas-body">
+            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Dropdown
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                  <li>
+                    <hr class="dropdown-divider">
+                  </li>
+                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </li>
+            </ul>
+            <form class="d-flex" role="search">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </nav>
+<!--navbar end-->
 
 
+<!--adding slider start-->
 
-</div><!--container end-->
-
-
-
-</div><!--top finish-->
-
-
-<div id="navbar" class="navbar navbar-default"><!--navbar navbar default-start-->
-
-<div class="container"><!--container start-->
+<div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: First slide" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#555" dy=".3em">First slide</text></svg>
     
-</div><!--container end-->
+        </div>
+        <div class="carousel-item">
+          <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Second slide" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#666"></rect><text x="50%" y="50%" fill="#444" dy=".3em">Second slide</text></svg>
+    
+        </div>
+        <div class="carousel-item">
+          <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Third slide" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#555"></rect><text x="50%" y="50%" fill="#333" dy=".3em">Third slide</text></svg>
+    
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+    
 
-<div class="navbar-header"><!--navbar-header start-->
+<!--adding slider end-->
 
-<a href="index.php" class="navbar-brand home"><!--navbar-brand home start-->
-
-<img src="#" alt="KC-shop store logo" class="hidden-xs">
-<!--desktop logo to be hidden once a certain pixel dimension is reached-->
-
-<img src="#" alt="KC-shop store logo Smartphone" class="visible-xs">
-
-</a><!--navbar-brand home end-->
-
-<button class="navbar-toggle" data-toggle="collapse" data-target="#navigation" >
-
-<span class="sr-only">Toggle navigation</span>
-
-<i class="fa fa-align-justify"></i>  
-
-</button>
-
-<button class="navbar-toggle" data-toggle="collapse" data-target="#search">
-
-<span class="sr-only">Toggle search</span>
-
-<i class="fa fa-align-justify"></i>  
-
-</button>
-
-</div><!--navbar-header end-->
-
-</div><!--navbar navbar default end-->
-
-
-ken home
-
-
+ken home creating nav
 
 <!--SCRIPT start-->
+        <script src="js/script.js"></script>
         <script src="js/Jquery-3.6.0.min.js"></script>
         <script src="js/bootstrap.bundle.min.js"></script>
+        
+        
 <!--SCRIPT end-->
 </body>
 </html>
