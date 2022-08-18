579b0e868bb25a397ab82bf89c93c01e768ab7bf
office code monarch <kenesau4763@gmail.com>
2022-08-18 17:15:30 +0300

    added an admin panel

---
+++ Admin-panel/products.php
@@ -0,0 +1,99 @@
+<?php
+
+@include 'config.php';
+
+if(isset($_POST['add_to_cart'])){
+
+   $product_name = $_POST['product_name'];
+   $product_price = $_POST['product_price'];
+   $product_image = $_POST['product_image'];
+   $product_quantity = 1;
+
+   $select_cart = mysqli_query($conn, "SELECT * FROM `cart` WHERE name = '$product_name'");
+
+   if(mysqli_num_rows($select_cart) > 0){
+      $message[] = 'product already added to cart';
+   }else{
+      $insert_product = mysqli_query($conn, "INSERT INTO `cart`(name, price, image, quantity) VALUES('$product_name', '$product_price', '$product_image', '$product_quantity')");
+      $message[] = 'product added to cart succesfully';
+   }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>products</title>
+
+<!-- links for css -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link rel="stylesheet" href="css/style.css">
+
+<!-- links for css end -->
+
+</head>
+<body>
+   
+<?php
+
+if(isset($message)){
+   foreach($message as $message){
+      echo '<div class="message"><span>'.$message.'</span> <i class="fas fa-times" onclick="this.parentElement.style.display = `none`;"></i> </div>';
+   };
+};
+
+?>
+
+<?php include 'header.php'; ?>
+
+<div class="container">
+
+<section class="products">
+
+   <h1 class="heading">latest products</h1>
+
+   <div class="box-container">
+
+      <?php
+      
+      $select_products = mysqli_query($conn, "SELECT * FROM `products`");
+      if(mysqli_num_rows($select_products) > 0){
+         while($fetch_product = mysqli_fetch_assoc($select_products)){
+      ?>
+
+      <form action="" method="post">
+         <div class="box">
+            <img src="uploaded_img/<?php echo $fetch_product['image']; ?>" alt="">
+            <h3><?php echo $fetch_product['name']; ?></h3>
+            <div class="price">$<?php echo $fetch_product['price']; ?>/-</div>
+            <input type="hidden" name="product_name" value="<?php echo $fetch_product['name']; ?>">
+            <input type="hidden" name="product_price" value="<?php echo $fetch_product['price']; ?>">
+            <input type="hidden" name="product_image" value="<?php echo $fetch_product['image']; ?>">
+            <input type="submit" class="btn" value="add to cart" name="add_to_cart">
+         </div>
+      </form>
+
+      <?php
+         };
+      };
+      ?>
+
+   </div>
+
+</section>
+
+</div>
+
+
+
+<!-- links for javascript -->
+<script src="jss/scriptss.js"></script>
+
+
+</body>
+</html>
