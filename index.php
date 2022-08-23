be8a7005a63e3242bbcfffccb490e59744bb3ad2
office code monarch <kenesau4763@gmail.com>
2022-08-23 18:07:08 +0300

    added header and footer plus nav bar 23/08/22

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ session_start();
 
 if (isset($_SESSION["user_id"])) {
     
-    $mysqli = require __DIR__ . "/database.php";
+    $mysqli = require __DIR__ . "../admin/signup/database.php";
     
     $sql = "SELECT * FROM user
             WHERE id = {$_SESSION["user_id"]}";
@@ -30,11 +30,11 @@ if (isset($_SESSION["user_id"])) {
         
         <p>Hello <?= htmlspecialchars($user["name"]) ?></p>
         
-        <p><a href="logout.php">Log out</a></p>
+        <p><a href="../admin/signup/logout.php">Log out</a></p>
         
     <?php else: ?>
         
-        <p><a href="login.php">Log in</a> or <a href="signup.html">sign up</a></p>
+        <p><a href="../admin/signup/login.php">Log in</a> or <a href="../admin/signup/signup.html">sign up</a></p>
         
     <?php endif; ?>
     
